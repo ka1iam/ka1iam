aa0c99a188105f7cbd5ee17902e62d468fb5cc0e
Kai Castelo <l1iamdpr@gmail.com>
2026-05-16 16:40:09 -0400

    feat: codigo pra contagem de numeros

---
+++ code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    printf("contando numeros pares ate %d:\n", limite);
+
+    //loop que passa por todos os numeros de 1 a 10 
+    for (int i = 0; i <= limite; i++){
+     
+        printf("%d\n", i);
+}
